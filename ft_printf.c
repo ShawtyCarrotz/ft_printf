3875d231fab819e1736c246e04ff93aa0398983e
Ines Pais <ines.pais17@gmail.com>
2024-06-27 20:03:21 +0100

    updated ft_printf

--- ft_printf.c
+++ ft_printf.c
@@ -10,8 +10,39 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
+#include "libft.h"
 #include <stdarg.h>
+
+int	printformat(char specifier, va_list args)
+{
+	unsigned int	count;
+
+	count = 0;
+	if (specifier == 'c')
+	{
+		ft_putchar_fd(va_args(args, int), 1);
+		count = 1;
+	}
+	else if (specifier == 's')
+	{
+		ft_putstr_fd(va_args(args, char *), 1);
+		count = ft_strlen();
+	}
+	else if (specifier == 'd' || specifier == 'i')
+		ft_putnbr_fd(va_args(args, int), 1);
+	else if (specifier == 'u')
+		ft_putnbr_fd(va_args(args, unsigned int), 1);
+	else if (specifier == 'x')
+		va_args(args, int);
+	else if (specifier == 'X')
+		va_args(args, int);
+	//if there's a char make it upper case*/
+	else if (specifier == 'p')
+		va_args(args, int);
+		//ft_printsignedint();*/
+
+	return (count);
+}
 
 int	ft_printf(const char *format, ...)
 {
@@ -25,27 +56,12 @@ int	ft_printf(const char *format, ...)
 	while (format[i])
 	{
 		if (format[i] != '%')
-			write(1, format[i], 1);
+			count += write(1, format[i], 1);
 		else
 		{
-			if (format[i + 1] == 'd' || format[i + 1] == 'i')
-				ft_putnbr_fd(va_args(args, int), 1);
-			else if (format[i + 1] == 'u')
-				ft_putnbr_fd(va_args(args, unsigned int), 1);
-			else if (format[i + 1] == 'x')
-				va_args(args, int);
-			else if (format[i + 1] == 'X')
-				va_args(args, int);
-			//if there's a char make it upper case
-			else if (format[i + 1] == 's')
-				ft_putstr_fd(va_args(args, char *));
-			else if (format[i + 1] == 'c')
-				ft_putchar_fd(va_args(args, char), 1);
-			else if (format[i + 1] == 'p')
-				va_args(args, int);
-				//ft_printsignedint();
+			i++;
+			count += printformat(format[i], args);
 		}
-		count++;
 		i++;
 	}
 	va_end(args);
